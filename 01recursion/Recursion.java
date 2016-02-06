4e1df24619aae92695ce8f777499bb5949738bae
Will Ostlund <wostlund@stuy.edu>
2016-02-06 12:44:57 -0500

    fixed error when 0 is entered

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -5,14 +5,14 @@ public class Recursion implements hw01{
     }
    
     public double sqrt(double n){
-	if(n<0){
-	    throw(new IllegalArgumentException("The real square root of a negative number does not exist"));
+	if(n<=0){
+	    throw(new IllegalArgumentException("The square root function only applies to positive numbers"));
 	}
 	return sqrtHelper(n, 1);
     }
 
     public double sqrtHelper(double n, double guess){
-	if(Math.abs((n-guess*guess)/n * 100) <= 0.0000001){
+	if(Math.abs((n-guess*guess)/n * 100) <= 0.000000001){
 	    return guess;
 	}
 	return sqrtHelper(n, (n/guess + guess)/2);
