95e7daf33b4a2931888ed27344dae5eafc3e90bf
Will Ostlund <wostlund@stuy.edu>
2016-02-06 12:50:51 -0500

    Added an exception at zero

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -5,14 +5,16 @@ public class Recursion implements hw01{
     }
    
     public double sqrt(double n){
-	if(n<=0){
+	if(n<0){
 	    throw(new IllegalArgumentException("The square root function only applies to positive numbers"));
+	}else if(n==0){
+	    return 0;
 	}
 	return sqrtHelper(n, 1);
     }
-
+    
     public double sqrtHelper(double n, double guess){
-	if(Math.abs((n-guess*guess)/n * 100) <= 0.000000001){
+	if(Math.abs((n-guess*guess)/n * 100) <= 0.0000001){
 	    return guess;
 	}
 	return sqrtHelper(n, (n/guess + guess)/2);
