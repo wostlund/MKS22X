8e0183335a66d63204e258b462f4c02705acbc3c
Will Ostlund <wostlund@stuy.edu>
2016-03-08 13:16:32 -0500

    Started HW

---
+++ 07quickSearch/Quick.java
@@ -0,0 +1,41 @@
+public class Quick{
+
+    public static int quickselect(int[]data, int place){
+	return quickselect(data, 0, data.length, place);
+    }
+
+    public static int quickselect(int[]data, int start, int end, int place){
+	int u = (int)(Math.random()*(data.length - start - end)) + start;
+	int j = partition(data, start, end, data[u]);
+	if (j == place){
+	    return data[j];
+	}else if(j > end - place){
+	    return quickselect(data, start, j, place);
+	}
+	return quickselect(data, j+1, end, place);
+    }
+
+    public static int partition(int[]data, int start, int end, int num){
+	int[]copy = new int[end-start];
+	int place = 0;
+	for(int i = start; i < end; i++){
+	    if(data[i] < num){
+		copy[place] = data[i];
+		place++;
+	    }
+	}
+	copy[place] = num;
+	int k = place + start;
+	place++;
+	for(int i = start; i < end; i++){
+	    if(data[i] >= num){
+		copy[place] = data[i];
+		place++;
+	    }
+	}
+	for(int i = 0; i < copy.length; i++){
+	    data[i + start] = copy[i];
+	}
+	return k;
+    }
+}
