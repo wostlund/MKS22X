54286e82e1ffaa45c9d7c2fe0ff0cb78ec21337b
Will Ostlund <wostlund@stuy.edu>
2016-03-28 22:35:11 -0400

    Hw 09 is done

---
+++ 09StackQueue/MyStack.java
@@ -0,0 +1,28 @@
+public class MyStack<T>{
+
+    private MyLinkedList<T> data;
+
+    public MyStack(){
+	data = new MyLinkedList<T>();
+    }
+
+    public void push(T item){
+	data.add(0,item);
+    }
+
+    public T pop(){
+	return data.remove(0);
+    }
+
+    public T peep(){
+	return data.get(0);
+    }
+
+    public int size(){
+	return data.size();
+    }
+
+    public boolean isEmpty(){
+	return data.size()==0;
+    }
+}
