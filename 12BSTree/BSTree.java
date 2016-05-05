55c0c4079504377793812db631efccadade65eeb
Will Ostlund <wostlund@stuy.edu>
2016-05-05 02:59:53 -0400

    Finished with everything but remove

--- 12BSTree/BSTree.java
+++ 12BSTree/BSTree.java
@@ -1,12 +1,22 @@
-public class BSTree<T extends comparable<T>>{
+import java.io.*;
+
+public class BSTree<T extends Comparable<T>>{
     private class Node{
 	private T data;
 	private Node left;
 	private Node right;
-	private int height;
 
+	public Node(T value){
+	    data = value;
+	    
+	}
+	
 	public boolean hasChildren(){
 	    return !(left != null && right != null);
+	}
+
+	public T getValue(){
+	    return data;
 	}
 
 	public Node getRight(){
@@ -32,26 +42,48 @@ public class BSTree<T extends comparable<T>>{
 
     private Node root;
 
+
     //OUTER methods here are wrapper methods for the root
-    public getHeight(){
+    public int getHeight(){
 	return getHeight(root, 0);
     }
 
-    public int getHeight(Node k, int height){
+    private int getHeight(Node k, int height){
 	if(k == null){
 	    return height;
 	}
 	int g = getHeight(k.getLeft(), height+1);
-	int k = getHeight(k.getRight(), height+1);
-	if(g > k){
+	int ka = getHeight(k.getRight(), height+1);
+	if(g > ka){
 	    return g;
 	}
-	return k;
+	return ka;
     }
 
     public void add(T value){
-	//check for empty before you do things with root.
+	add(value, root);
     }
+
+    private void add(T value, Node current){
+	if(current == null){
+	    root = new Node(value);
+	    return;
+	}
+	if(value.compareTo(current.getValue()) > 0){
+	    if(current.getRight() == null){
+		current.setRight(new Node(value));
+		return;
+	    }
+	    add(value, current.getRight());
+	    return;
+	}
+	if(current.getLeft() == null){
+	    current.setLeft(new Node(value));
+	    return;
+	}
+	add(value,current.getLeft());
+    }
+    
     public String toString(){
 	return toString(root);
     }
@@ -60,17 +92,32 @@ public class BSTree<T extends comparable<T>>{
 	if (k == null){
 	    return " _"; 
 	}
-	return "" + k.getValue() + " " + toString(k.getLeft()) + " " + toString(k.getRight());
+	return " " + k.getValue() + " " + toString(k.getLeft()) + " " + toString(k.getRight());
     }
     
     public boolean contains(T value){
-	//check for empty before you do things with root.
-	return false;
+        return contains(value, root);
     }
-}import java.io.*;
 
-public class BSTree<T implements Comparable<T>>{
+    private boolean contains(T value, Node current){
+	if (current == null){
+	    return false;
+	}
+	else if(current.getValue().compareTo(value) == 0){
+	    return true;
+	}
+	return contains(value, current.getLeft()) || contains(value, current.getRight());
+    }
 
-    private TreeNode root;
-    
+    public static void main(String[]args){
+	BSTree<Integer> mark = new BSTree<Integer>();
+        mark.add(new Integer(8));
+	mark.add(new Integer(3));
+	mark.add(new Integer(1));
+	mark.add(new Integer(3));
+	mark.add(new Integer(6));
+	mark.add(new Integer(4));
+	System.out.println(mark);
+	System.out.println(mark.getHeight());
+    }
 }
